bed29969a22cab253c9c28955ea138a809b172fb
reachosen <reachosen@gmail.com>
2025-11-30 22:15:39 -0600

    chore(cleanup): remove unused v1 planner agents and archive

--- clinical-planner-cli/archive/validate_v711.ts
+++
@@ -1,6 +0,0 @@
-import { validatePlanV71, printValidationResults } from './planner/externalValidator';
-import * as fs from 'fs';
-
-const plan = JSON.parse(fs.readFileSync('./planner_plan.json', 'utf-8'));
-const result = validatePlanV71(plan);
-printValidationResults(result);
